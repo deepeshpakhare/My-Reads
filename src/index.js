515f9048efe8b43cd12dfbe5d983c0e124692e3f
deepeshpakhare <deepeshpakhare@gmail.com>
2023-12-07 19:02:48 +0530

    feat: adjustment of correct paths

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Search from './Components/Serach/Search';
+import Search from './Components/Search/Search';
 import {
   createBrowserRouter,
   RouterProvider,
